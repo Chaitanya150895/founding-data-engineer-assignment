82843fe04a288e9bd8fa36b79afd882d49657332
Chaitanya150895 <48850034+Chaitanya150895@users.noreply.github.com>
2026-01-21 17:11:39 +0530

    Simplify logger setup using basicConfig
    Refactor logger setup to use basicConfig for simplicity.

--- utils/logger.py
+++ utils/logger.py
@@ -1,13 +1,9 @@
-"""
-logger.py - basic logging setup
-"""
 import logging
-def get_logger(name=__name__):
-    logger = logging.getLogger(name)
-    if not logger.handlers:
-        handler = logging.StreamHandler()
-        fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        handler.setFormatter(logging.Formatter(fmt))
-        logger.addHandler(handler)
-        logger.setLevel(logging.INFO)
-    return logger
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.StreamHandler()]
+)
+
+logger = logging.getLogger("pipeline")
